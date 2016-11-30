edd93548e56b38401957b38e7dd5fc87ce406965
sulvto <lpetrel@outlook.com>
2016-11-30 14:26:27 +0800

    Parser.jj(CaseNode ContinueNode GotoNode)

--- src/main/java/ast/Location.java
+++ src/main/java/ast/Location.java
@@ -4,7 +4,26 @@ package ast;
  * Created by sulvto on 16-11-14.
  */
 public class Location {
+    protected String sourceName;
+    protected CflatToken token;
+
+    public Location(String sourceName, Token token) {
+        this(sourceName, new CflatToken(token));
+    }
+
+    public Location(String sourceName, CflatToken token) {
+        this.sourceName = sourceName;
+        this.token = token;
+    }
+
+
+
     public CflatToken[] token() {
         return new CflatToken[0];
+    }
+
+    @Override
+    public String toString() {
+        return sourceName+":"+token.lineno();
     }
 }
